d2cbf3c3a0caa6c19f8f78ed9a05de9bac712dc5
sowmyadev-kota <kvsowmyasri21@gmail.com>
2024-03-26 08:03:31 -0700

    added gettopic mapping

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,9 +1,9 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +17,13 @@ public class TopicController {
 	public List<Topic> getAllTopics()
 {
 		return topicService.getAllTopics();
+		
+}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+{
+		return topicService.getTopic(id);
 		
 }
 }
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -18,4 +18,9 @@ public class TopicService {
 {
 		return topics;
 		}
+
+	public Topic getTopic(String id) {
+		// TODO Auto-generated method stub
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
 }
