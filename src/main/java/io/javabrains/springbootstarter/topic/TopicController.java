59f2b8f496ae291cf817644dec611f8ea6941f69
sowmyadev-kota <kvsowmyasri21@gmail.com>
2024-03-26 09:01:49 -0700

    added put method for updating topic

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -32,4 +32,8 @@ public class TopicController {
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
+	@RequestMapping(method=RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+		topicService.updateTopic(topic,id);
+	}
 }
